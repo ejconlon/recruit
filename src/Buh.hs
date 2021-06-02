3cf39f6dcf8aa9c2ae814a91e37b524a315df47e
Eric Conlon <ejconlon@gmail.com>
2021-06-02 14:32:00 -0700

    logging

--- src/Buh.hs
+++ src/Buh.hs
@@ -8,9 +8,11 @@ https://hackage.haskell.org/package/brick-0.62/docs/Brick-Main.html#t:App
 -}
 module Buh where
 
-import Brick (App (..), BrickEvent (..), CursorLocation, EventM, Next, ViewportType (..), Widget, attrMap, continue,
-              customMain, getVtyHandle, halt, showCursorNamed, txt, vBox, vLimit, viewport)
+import Brick (App (..), AttrMap, AttrName, BrickEvent (..), CursorLocation, EventM, Location (..), Next,
+              ViewportType (..), Widget, attrMap, continue, customMain, getVtyHandle, halt, showCursor, showCursorNamed,
+              textWidth, txt, vBox, vLimit, viewport, visibleRegion, withAttr)
 import Brick.BChan (BChan, newBChan, readBChan, writeBChan)
+import Brick.Util (on)
 import Brick.Widgets.Border (hBorder)
 import Control.Concurrent.Async (race)
 import Control.Monad (forever, when)
@@ -19,10 +21,13 @@ import Data.Either (isLeft)
 import Data.IORef (IORef, modifyIORef', newIORef, readIORef, writeIORef)
 import Data.List (intersperse)
 import qualified Graphics.Vty as V
+import LittleLogger.Manual (SimpleLogAction, fileSimpleLogAction, logDebug)
 -- import Data.Sequence (Seq)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Data.Text.Zipper (TextZipper)
+import qualified Data.Text.Zipper as Z
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 import System.Exit (die)
@@ -111,20 +116,48 @@ data St = St
   , stEdMode :: !EdMode
   , stReqIndex :: !Int
   , stFilename :: !(Maybe FilePath)
-  , stEditorContents :: !Text
+  , stEditorContents :: !(TextZipper Text)
   , stStatusLine :: !Text
   , stCommandLine :: !Text
   } deriving (Eq, Show)
 
 mkSt :: St
-mkSt = St NameEditor EdModeNormal 0 Nothing "" "" ""
+mkSt = St NameEditor EdModeNormal 0 Nothing (Z.textZipper [] Nothing) "" ""
 
 stacked :: [Widget Name] -> [Widget Name]
 stacked widgets = [vBox (intersperse hBorder widgets)]
 
+charAtCursor :: TextZipper Text -> Maybe Text
+charAtCursor z =
+    let col = snd (Z.cursorPosition z)
+        curLine = Z.currentLine z
+        toRight = T.drop col curLine
+    in if T.length toRight > 0
+       then Just (T.take 1 toRight)
+       else Nothing
+
+renderEditor :: St -> Widget Name
+renderEditor st =
+    let z = stEditorContents st
+        emo = stEdMode st
+        foc = emo /= EdModeCommand
+        (row, col) = Z.cursorPosition z
+        toLeft = T.take col (Z.currentLine z)
+        cursorLoc = Location (textWidth toLeft, row)
+        limit = maybe id vLimit (Z.getLineLimit z)
+        atChar = charAtCursor z
+        atCharWidth = maybe 1 textWidth atChar
+    in withAttr plainOffAttr $
+       limit $
+       viewport NameEditor Both $
+       (if foc then showCursor NameEditor cursorLoc else id) $
+       visibleRegion cursorLoc (atCharWidth, 1) $
+       vBox . fmap txt $
+       Z.getText z
+
 myDraw :: St -> [Widget Name]
 myDraw st =
-  let editorArea = viewport NameEditor Both (txt (stEditorContents st))
+  let editorArea = renderEditor st
       statusArea = vLimit 1 (viewport NameStatus Horizontal (txt (stStatusLine st)))
       commandArea = vLimit 1 (viewport NameCommand Horizontal (txt (stCommandLine st)))
   in stacked [editorArea, statusArea, commandArea]
@@ -145,7 +178,7 @@ writeStatus :: MonadIO m => IORef St -> Text -> m ()
 writeStatus ref line = liftIO (modifyIORef' ref (\st -> st { stStatusLine = line }))
 
 setContents :: MonadIO m => IORef St -> Text -> m ()
-setContents ref contents = liftIO (modifyIORef' ref (\st -> st { stEditorContents = contents }))
+setContents ref contents = liftIO (modifyIORef' ref (\st -> st { stEditorContents = Z.textZipper (T.lines contents) Nothing }))
 
 runSt :: MonadIO m => St -> (IORef St -> m a) -> m (a, St)
 runSt st0 f = do
@@ -195,6 +228,9 @@ setEdMode ref emo = do
   liftIO (modifyIORef' ref (\st -> st { stFocus = focus, stEdMode = emo, stCommandLine = comLine }))
   writeStatus ref (text <> " mode")
 
+editContents :: MonadIO m => IORef St -> (TextZipper Text -> TextZipper Text) -> m ()
+editContents ref f = liftIO (modifyIORef' ref (\st -> st { stEditorContents = f (stEditorContents st) }))
+
 myHandle :: BChan Req -> St -> BrickEvent Name Ev -> EventM Name (Next St)
 myHandle _ st e = withNextSt st $ \ref -> do
   case e of
@@ -213,6 +249,22 @@ myHandle _ st e = withNextSt st $ \ref -> do
         EdModeInsert ->
           case (key, mods) of
             (V.KEsc, []) -> setEdMode ref EdModeNormal
+            (V.KChar 'a', [V.MCtrl]) -> editContents ref Z.gotoBOL
+            (V.KChar 'e', [V.MCtrl]) -> editContents ref Z.gotoEOL
+            (V.KChar 'd', [V.MCtrl]) -> editContents ref Z.deleteChar
+            (V.KChar 'k', [V.MCtrl]) -> editContents ref Z.killToEOL
+            (V.KChar 'u', [V.MCtrl]) -> editContents ref Z.killToBOL
+            (V.KEnter, []) -> editContents ref Z.breakLine
+            (V.KDel, []) -> editContents ref Z.deleteChar
+            (V.KChar c, []) | c /= '\t' -> editContents ref (Z.insertChar c)
+            -- Tab chars need to be rendered as spaces... too much trouble
+            (V.KUp, []) -> editContents ref Z.moveUp
+            (V.KDown, []) -> editContents ref Z.moveDown
+            (V.KLeft, []) -> editContents ref Z.moveLeft
+            (V.KRight, []) -> editContents ref Z.moveRight
+            (V.KBS, []) -> editContents ref Z.deletePrevChar
+            (V.KHome, []) -> editContents ref Z.gotoBOL
+            (V.KEnd, []) -> editContents ref Z.gotoEOL
             _ -> pure ()
         EdModeCommand ->
           case (key, mods) of
@@ -233,18 +285,32 @@ myHandle _ st e = withNextSt st $ \ref -> do
       pure NextOpContinue
     _ -> pure NextOpContinue
 
-mkApp :: BChan Req -> Maybe FilePath -> App St Ev Name
-mkApp reqChan mfp = App
+plainOnAttr :: AttrName
+plainOnAttr = "plain" <> "on"
+
+plainOffAttr :: AttrName
+plainOffAttr = "plain" <> "off"
+
+myAttrMap :: AttrMap
+myAttrMap = attrMap V.defAttr
+  [ (plainOnAttr, V.black `on` V.white)
+  , (plainOffAttr, V.white `on` V.black)
+  ]
+
+mkApp :: SimpleLogAction -> BChan Req -> Maybe FilePath -> App St Ev Name
+mkApp _ reqChan mfp = App
   { appDraw = myDraw
   , appChooseCursor = myChoose
   , appHandleEvent = myHandle reqChan
   , appStartEvent = myStart reqChan mfp
-  , appAttrMap = const (attrMap V.defAttr [])
+  , appAttrMap = const myAttrMap
   }
 
-worker :: BChan Req -> BChan Ev -> IO ()
-worker reqChan evChan = forever $ do
-  Req n reqb <- liftIO (readBChan reqChan)
+worker :: SimpleLogAction -> BChan Req -> BChan Ev -> IO ()
+worker action reqChan evChan = forever $ do
+  req <- liftIO (readBChan reqChan)
+  logDebug action ("Worker req: " <> T.pack (show req))
+  let Req n reqb = req
   resb <- case reqb of
     ReqBodyPing -> pure ResBodyPong
     ReqBodyBoom -> error "Boom!"
@@ -266,11 +332,12 @@ exe = do
   let maxLen = 1000
   reqChan <- newBChan maxLen
   evChan <- newBChan maxLen
-  let app = mkApp reqChan mfn
   let vtyBuilder = V.mkVty V.defaultConfig
   firstVty <- vtyBuilder
-  let runUi = customMain firstVty vtyBuilder (Just evChan) app mkSt
-  let runWorker = worker reqChan evChan
-  putStrLn "Starting app"
-  _ <- race runUi runWorker
-  putStrLn "Stopped app"
+  fileSimpleLogAction "/tmp/buh" $ \action -> do
+    let app = mkApp action reqChan mfn
+        runUi = customMain firstVty vtyBuilder (Just evChan) app mkSt
+        runWorker = worker action reqChan evChan
+    putStrLn "Starting app"
+    _ <- race runUi runWorker
+    putStrLn "Stopped app"
