2c7ec2030f6a873add443e267b19b4408d1d05b0
Eric Conlon <ejconlon@gmail.com>
2021-06-02 20:05:51 -0700

    editing

--- src/Buh.hs
+++ src/Buh.hs
@@ -9,15 +9,16 @@ https://hackage.haskell.org/package/brick-0.62/docs/Brick-Main.html#t:App
 module Buh where
 
 import Brick (App (..), AttrMap, AttrName, BrickEvent (..), CursorLocation, EventM, Location (..), Next,
-              ViewportType (..), Widget, attrMap, continue, customMain, getVtyHandle, halt, showCursor, showCursorNamed,
-              textWidth, txt, vBox, vLimit, viewport, visibleRegion, withAttr)
+              ViewportType (..), Widget, attrMap, continue, customMain, getVtyHandle, hBox, halt, showCursor,
+              showCursorNamed, textWidth, txt, vBox, vLimit, viewport, visibleRegion, withAttr)
 import Brick.BChan (BChan, newBChan, readBChan, writeBChan)
 import Brick.Util (on)
 import Brick.Widgets.Border (hBorder)
 import Control.Concurrent.Async (race)
-import Control.Monad (forever, when)
+import Control.Monad (forever, join, when)
 import Control.Monad.IO.Class (MonadIO (..))
 import Data.Either (isLeft)
+import Data.Functor (($>))
 import Data.IORef (IORef, modifyIORef', newIORef, readIORef, writeIORef)
 import Data.List (intersperse)
 import qualified Graphics.Vty as V
@@ -112,20 +113,30 @@ edModeText emo =
     EdModeCommand -> "Command"
 
 data St = St
-  { stFocus :: !Name
-  , stEdMode :: !EdMode
+  { stEdMode :: !EdMode
   , stReqIndex :: !Int
   , stFilename :: !(Maybe FilePath)
   , stEditorContents :: !(TextZipper Text)
   , stStatusLine :: !Text
-  , stCommandLine :: !Text
+  , stCommandLine :: !(TextZipper Text)
   } deriving (Eq, Show)
 
+stFocus :: St -> Name
+stFocus = edModeFocus . stEdMode
+
+emptyZipper :: TextZipper Text
+emptyZipper = Z.textZipper [] Nothing
+
 mkSt :: St
-mkSt = St NameEditor EdModeNormal 0 Nothing (Z.textZipper [] Nothing) "" ""
+mkSt = St EdModeNormal 0 Nothing emptyZipper "" emptyZipper
 
 stacked :: [Widget Name] -> [Widget Name]
 stacked widgets = [vBox (intersperse hBorder widgets)]
+
+-- TODO there should be a way to cache this...
+-- (And support tab width > 1)
+saniTxt :: Text -> Widget Name
+saniTxt t = txt (if T.any (== '\t') t then T.map (\c -> if c == '\t' then ' ' else c) t else t)
 
 charAtCursor :: TextZipper Text -> Maybe Text
 charAtCursor z =
@@ -136,7 +147,7 @@ charAtCursor z =
        then Just (T.take 1 toRight)
        else Nothing
 
-renderEditor :: St -> Widget Name
+renderEditor :: St -> [Widget Name]
 renderEditor st =
     let z = stEditorContents st
         emo = stEdMode st
@@ -146,19 +157,41 @@ renderEditor st =
         cursorLoc = Location (textWidth toLeft, row)
         atChar = charAtCursor z
         atCharWidth = maybe 1 textWidth atChar
-    in withAttr plainOffAttr $
-       viewport NameEditor Both $
-       (if foc then showCursor NameEditor cursorLoc else id) $
-       visibleRegion cursorLoc (atCharWidth, 1) $
-       vBox . fmap txt $
-       Z.getText z
+        widget = withAttr plainOffAttr $
+          viewport NameEditor Both $
+          (if foc then showCursor NameEditor cursorLoc else id) $
+          visibleRegion cursorLoc (atCharWidth, 1) $
+          vBox . fmap saniTxt $
+          Z.getText z
+    in [widget]
+
+renderStatus :: St -> [Widget Name]
+renderStatus st =
+  let line = stStatusLine st
+  in [vLimit 1 (viewport NameStatus Horizontal (saniTxt line)) | not (T.null line) ]
+
+renderCommand :: St -> [Widget Name]
+renderCommand st =
+    let z = stCommandLine st
+        emo = stEdMode st
+        foc = emo == EdModeCommand
+        (row, col) = Z.cursorPosition z
+        toLeft = T.take col (Z.currentLine z)
+        cursorLoc = Location (textWidth toLeft, row)
+        atChar = charAtCursor z
+        atCharWidth = maybe 1 textWidth atChar
+        innerWidget =
+          withAttr plainOffAttr $
+            viewport NameCommand Both $
+            (if foc then showCursor NameCommand cursorLoc else id) $
+            visibleRegion cursorLoc (atCharWidth, 1) $
+            vBox . fmap saniTxt $
+            Z.getText z
+        outerWidget = hBox [txt ":", innerWidget]
+    in [vLimit 1 outerWidget | foc]
 
 myDraw :: St -> [Widget Name]
-myDraw st =
-  let editorArea = renderEditor st
-      statusArea = vLimit 1 (viewport NameStatus Horizontal (txt (stStatusLine st)))
-      commandArea = vLimit 1 (viewport NameCommand Horizontal (txt (stCommandLine st)))
-  in stacked [editorArea, statusArea, commandArea]
+myDraw st = stacked (join [renderEditor st, renderStatus st, renderCommand st])
 
 myChoose :: St -> [CursorLocation Name] -> Maybe (CursorLocation Name)
 myChoose = showCursorNamed . stFocus
@@ -220,54 +253,109 @@ myStart reqChan mfp st = do
 
 setEdMode :: MonadIO m => IORef St -> EdMode -> m ()
 setEdMode ref emo = do
-  let focus = edModeFocus emo
-      comLine = if focus == NameCommand then ":" else ""
-      text = edModeText emo
-  liftIO (modifyIORef' ref (\st -> st { stFocus = focus, stEdMode = emo, stCommandLine = comLine }))
-  writeStatus ref (text <> " mode")
+  liftIO $ modifyIORef' ref $ \st ->
+    -- If moving away from command focus, clear command line
+    let comLine = if emo /= EdModeCommand && stEdMode st == EdModeCommand then Z.textZipper [] Nothing else stCommandLine st
+    in st { stEdMode = emo, stCommandLine = comLine }
+  writeStatus ref (edModeText emo <> " mode")
 
 editContents :: MonadIO m => IORef St -> (TextZipper Text -> TextZipper Text) -> m ()
 editContents ref f = liftIO (modifyIORef' ref (\st -> st { stEditorContents = f (stEditorContents st) }))
 
+editCommand :: MonadIO m => IORef St -> (TextZipper Text -> TextZipper Text) -> m ()
+editCommand ref f = liftIO (modifyIORef' ref (\st -> st { stCommandLine = f (stCommandLine st) }))
+
+data Clear = ClearYes | ClearNo deriving (Eq, Show)
+
 myHandle :: BChan Req -> St -> BrickEvent Name Ev -> EventM Name (Next St)
 myHandle _ st e = withNextSt st $ \ref -> do
   case e of
-    -- Hard quit on mac: Left option + escape
+    -- Hard quit: Left option + escape, or ctrl-c
     VtyEvent (V.EvKey V.KEsc [V.MMeta]) -> pure NextOpHalt
+    VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl]) -> pure NextOpHalt
     -- Handle rest of keyboard input
     VtyEvent (V.EvKey key mods) -> do
       emo <- getsSt ref stEdMode
-      case emo of
+      clear <- case emo of
         EdModeNormal ->
           case (key, mods) of
-            (V.KEsc, []) -> setEdMode ref EdModeNormal
-            (V.KChar ':', []) -> setEdMode ref EdModeCommand
-            (V.KChar 'i', []) -> setEdMode ref EdModeInsert
-            _ -> pure ()
+            -- Mode switching
+            (V.KEsc, []) -> setEdMode ref EdModeNormal $> ClearNo
+            (V.KChar ':', []) -> setEdMode ref EdModeCommand $> ClearNo
+            (V.KChar 'i', []) -> setEdMode ref EdModeInsert $> ClearNo
+            -- Chorded + special key movement
+            (V.KChar 'a', [V.MCtrl]) -> editContents ref Z.gotoBOL $> ClearYes
+            (V.KChar 'e', [V.MCtrl]) -> editContents ref Z.gotoEOL $> ClearYes
+            (V.KChar 'k', [V.MCtrl]) -> editContents ref Z.killToEOL $> ClearYes
+            (V.KChar 'u', [V.MCtrl]) -> editContents ref Z.killToBOL $> ClearYes
+            (V.KUp, []) -> editContents ref Z.moveUp $> ClearYes
+            (V.KChar 'p', [V.MCtrl]) -> editContents ref Z.moveUp $> ClearYes
+            (V.KDown, []) -> editContents ref Z.moveDown $> ClearYes
+            (V.KChar 'n', [V.MCtrl]) -> editContents ref Z.moveDown $> ClearYes
+            (V.KLeft, []) -> editContents ref Z.moveLeft $> ClearYes
+            (V.KChar 'b', [V.MCtrl]) -> editContents ref Z.moveLeft $> ClearYes
+            (V.KRight, []) -> editContents ref Z.moveRight $> ClearYes
+            (V.KChar 'f', [V.MCtrl]) -> editContents ref Z.moveRight $> ClearYes
+            (V.KHome, []) -> editContents ref Z.gotoBOL $> ClearYes
+            (V.KEnd, []) -> editContents ref Z.gotoEOL $> ClearYes
+            -- Vim movement
+            (V.KChar 'h', []) -> editContents ref Z.moveLeft $> ClearYes
+            (V.KChar 'j', []) -> editContents ref Z.moveDown $> ClearYes
+            (V.KChar 'k', []) -> editContents ref Z.moveUp $> ClearYes
+            (V.KChar 'l', []) -> editContents ref Z.moveRight $> ClearYes
+            (V.KChar '0', []) -> editContents ref Z.gotoBOL $> ClearYes
+            (V.KChar '$', []) -> editContents ref Z.gotoEOL $> ClearYes
+            _ -> pure ClearNo
         EdModeInsert ->
           case (key, mods) of
-            (V.KEsc, []) -> setEdMode ref EdModeNormal
-            (V.KChar 'a', [V.MCtrl]) -> editContents ref Z.gotoBOL
-            (V.KChar 'e', [V.MCtrl]) -> editContents ref Z.gotoEOL
-            (V.KChar 'd', [V.MCtrl]) -> editContents ref Z.deleteChar
-            (V.KChar 'k', [V.MCtrl]) -> editContents ref Z.killToEOL
-            (V.KChar 'u', [V.MCtrl]) -> editContents ref Z.killToBOL
-            (V.KEnter, []) -> editContents ref Z.breakLine
-            (V.KDel, []) -> editContents ref Z.deleteChar
-            (V.KChar c, []) | c /= '\t' -> editContents ref (Z.insertChar c)
-            -- Tab chars need to be rendered as spaces... too much trouble
-            (V.KUp, []) -> editContents ref Z.moveUp
-            (V.KDown, []) -> editContents ref Z.moveDown
-            (V.KLeft, []) -> editContents ref Z.moveLeft
-            (V.KRight, []) -> editContents ref Z.moveRight
-            (V.KBS, []) -> editContents ref Z.deletePrevChar
-            (V.KHome, []) -> editContents ref Z.gotoBOL
-            (V.KEnd, []) -> editContents ref Z.gotoEOL
-            _ -> pure ()
+            -- Mode switching
+            (V.KEsc, []) -> setEdMode ref EdModeNormal $> ClearNo
+            -- Chorded + special key movement
+            (V.KChar 'a', [V.MCtrl]) -> editContents ref Z.gotoBOL $> ClearYes
+            (V.KChar 'e', [V.MCtrl]) -> editContents ref Z.gotoEOL $> ClearYes
+            (V.KChar 'k', [V.MCtrl]) -> editContents ref Z.killToEOL $> ClearYes
+            (V.KChar 'u', [V.MCtrl]) -> editContents ref Z.killToBOL $> ClearYes
+            (V.KUp, []) -> editContents ref Z.moveUp $> ClearYes
+            (V.KChar 'p', [V.MCtrl]) -> editContents ref Z.moveUp $> ClearYes
+            (V.KDown, []) -> editContents ref Z.moveDown $> ClearYes
+            (V.KChar 'n', [V.MCtrl]) -> editContents ref Z.moveDown $> ClearYes
+            (V.KLeft, []) -> editContents ref Z.moveLeft $> ClearYes
+            (V.KChar 'b', [V.MCtrl]) -> editContents ref Z.moveLeft $> ClearYes
+            (V.KRight, []) -> editContents ref Z.moveRight $> ClearYes
+            (V.KChar 'f', [V.MCtrl]) -> editContents ref Z.moveRight $> ClearYes
+            (V.KHome, []) -> editContents ref Z.gotoBOL $> ClearYes
+            (V.KEnd, []) -> editContents ref Z.gotoEOL $> ClearYes
+            -- Editing
+            (V.KEnter, []) -> editContents ref Z.breakLine $> ClearYes
+            (V.KDel, []) -> editContents ref Z.deleteChar $> ClearYes
+            (V.KChar 'd', [V.MCtrl]) -> editContents ref Z.deleteChar $> ClearYes
+            (V.KBS, []) -> editContents ref Z.deletePrevChar $> ClearYes
+            (V.KChar c, []) -> editContents ref (Z.insertChar c) $> ClearYes
+            _ -> pure ClearNo
         EdModeCommand ->
           case (key, mods) of
-            (V.KEsc, []) -> setEdMode ref EdModeNormal
-            _ -> pure ()
+            -- Mode switching
+            (V.KEsc, []) -> setEdMode ref EdModeNormal $> ClearNo
+            -- Chorded + special key movement
+            (V.KChar 'a', [V.MCtrl]) -> editCommand ref Z.gotoBOL $> ClearYes
+            (V.KChar 'e', [V.MCtrl]) -> editCommand ref Z.gotoEOL $> ClearYes
+            (V.KChar 'k', [V.MCtrl]) -> editCommand ref Z.killToEOL $> ClearYes
+            (V.KChar 'u', [V.MCtrl]) -> editCommand ref Z.killToBOL $> ClearYes
+            (V.KLeft, []) -> editCommand ref Z.moveLeft $> ClearYes
+            (V.KChar 'b', [V.MCtrl]) -> editCommand ref Z.moveLeft $> ClearYes
+            (V.KRight, []) -> editCommand ref Z.moveRight $> ClearYes
+            (V.KChar 'f', [V.MCtrl]) -> editCommand ref Z.moveRight $> ClearYes
+            (V.KHome, []) -> editCommand ref Z.gotoBOL $> ClearYes
+            (V.KEnd, []) -> editCommand ref Z.gotoEOL $> ClearYes
+            -- Editing
+            (V.KDel, []) -> editCommand ref Z.deleteChar $> ClearYes
+            (V.KChar 'd', [V.MCtrl]) -> editCommand ref Z.deleteChar $> ClearYes
+            (V.KBS, []) -> editCommand ref Z.deletePrevChar $> ClearYes
+            (V.KChar c, []) -> editCommand ref (Z.insertChar c) $> ClearYes
+            -- Sending command
+            -- TODO enter
+            _ -> pure ClearNo
+      when (clear == ClearYes) (writeStatus ref "")
       pure NextOpContinue
     -- Handle worker events
     AppEvent ev -> do
